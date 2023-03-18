70c0fd2a7ca0401441bf57ff30e43ef65755ce8d
Simon <simon@Simons-MacBook-Pro.local>
2023-03-18 15:10:05 +1300

    allure-reports HTML added

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -138,7 +138,11 @@ export const config: Options.Testrunner = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter
-    reporters: ['spec'],
+    reporters: ['spec',
+    ['allure', {
+    	outputDir: 'allure-results'}
+    ]
+    ],
 
 
     
